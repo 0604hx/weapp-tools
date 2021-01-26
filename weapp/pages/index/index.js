938168efec293d73499cd3bd6e573761580a3c47
0604hx <1053214511@qq.com>
2021-01-26 14:29:08 +0800

    :fire: 目录重构

--- weapp/pages/index/index.js
+++ weapp/pages/index/index.js
@@ -22,7 +22,7 @@ Page({
                     { name:"胎儿体重测算", icon:"胎儿", url:"/pages/daily/fetusWeight/fetus-weight"},
                     { name:"密码本", icon:"密码", url:"/pages/daily/password/password", dot: true},
                     { name:"时序数据", icon:"时序数据库", url:"/pages/daily/sequence/sequence", tip:"New"},
-                    { name:"支付截图工具", icon:"微信支付", url:"/pages/daily/screenMaker/maker", tip:"New"}
+                    { name:"截图生成器", icon:"生成支付单", url:"/pages/daily/screenMaker/maker", tip:"New"}
                     // { name:"HEIC转换", icon:"美术图片", onClick:"todo", dot: true},
                 ]
             },
